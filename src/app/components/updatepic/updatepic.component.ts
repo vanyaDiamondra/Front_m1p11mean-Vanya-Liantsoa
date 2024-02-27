b4cfe5d30428f2604d780a24fd7003ebfc2f90c3
liantsoa17 <liantsurakotonirina@gmail.com>
2024-02-27 13:54:56 +0300

    Update profile pic

--- src/app/components/updatepic/updatepic.component.ts
+++ src/app/components/updatepic/updatepic.component.ts
@@ -8,7 +8,7 @@ import { AngularFireStorage } from "@angular/fire/compat/storage"
   styleUrls: ['./updatepic.component.css']
 })
 export class UpdateComponent {
-  title = 'imageupload';
+  imageUrl = '';
 
   constructor(private fireStorage:AngularFireStorage){}
 
@@ -22,7 +22,6 @@ export class UpdateComponent {
     event.preventDefault();
     event.stopPropagation();
     const target = event.target as HTMLElement;
-    // Add visual indication when item is dragged over the drop target
     target.classList.add('drag-over');
   }
 
@@ -30,7 +29,6 @@ export class UpdateComponent {
     event.preventDefault();
     event.stopPropagation();
     const target = event.target as HTMLElement;
-    // Remove visual indication when item leaves the drop target
     target.classList.remove('drag-over');
   }
 
@@ -38,52 +36,54 @@ export class UpdateComponent {
     event.preventDefault();
     event.stopPropagation();
     const target = event.target as HTMLElement;
-    // Remove visual indication when item is dropped
     target.classList.remove('drag-over');
+
 
     const files = event.dataTransfer?.files;
     if (files && files.length > 0) {
        this.uploadFiles(files);
     }
 
-    // const files = event.dataTransfer?.files;
-    // if (files && files.length > 0) {
-    //   this.saveFiles(files);
-    // }
+    const imageView = document.getElementById("img-view");
+    if (imageView) {
+      imageView.style.backgroundImage = `url(${this.imageUrl})`;
+      imageView.textContent = "";
+      imageView.style.border = "0" ;
+    }
+
+
+
+
   }
 
   saveFiles(files: FileList) {
-    // Perform operations to save the dropped files
+
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
       console.log('Dropped file:', file.name);
-      // Here, you can upload the file to a server or process it as needed
+
     }
   }
   onDragStart(event: DragEvent) {
-    // Add any necessary data to the drag event
-    const target = event.target as HTMLImageElement; // Ensure the target is an <img> element
-    const imageUrl = target.src; // Get the source URL of the image
+
+    const target = event.target as HTMLImageElement;
+    const imageUrl = target.src;
     if (event.dataTransfer && imageUrl) {
       event.dataTransfer.setData('text/uri-list', imageUrl);
     }
   }
 
   onDragEnd(event: DragEvent) {
-    // Perform any cleanup tasks after dragging ends
+
   }
 
   async uploadFiles(files: FileList) {
     for (let i = 0; i < files.length; i++) {
       const file = files[i];
-      const filePath = `uploads/${file.name}`;
-      const fileRef = this.fireStorage.ref(filePath);
-      const task = this.fireStorage.upload(filePath, file);
-
-      // Wait for the upload task to complete
-      await task;
-
-      console.log('File uploaded successfully:', filePath);
+      const path = `${file.name}`;
+      const uploadTask =await this.fireStorage.upload(path,file);
+      const url = await uploadTask.ref.getDownloadURL();
+      this.imageUrl = url;
     }
   }
 
@@ -95,7 +95,13 @@ export class UpdateComponent {
       const path = `${file.name}`;
       const uploadTask =await this.fireStorage.upload(path,file);
       const url = await uploadTask.ref.getDownloadURL();
-      console.log(url);
+      this.imageUrl = url ;
+      const imageView = document.getElementById("img-view");
+      if (imageView) {
+        imageView.style.backgroundImage = `url(${this.imageUrl})`;
+        imageView.textContent = "";
+        imageView.style.border = "0" ;
+      }
     }
   }
 
